237ff2bcd5f1be70a078fcedb6001a2218ae0600
Yannick Müller <yannick.mueller@fusiva.ch>
2023-04-28 19:47:05 +0200

    updated functions to show vowels

--- src/app/state/appointment/appointment.reducer.ts
+++ src/app/state/appointment/appointment.reducer.ts
@@ -4,8 +4,25 @@ import { clear, loadAppointmentsSuccess } from "./appointment.action";
 
 const INITIAL_STATE: Array<Appointment> = [];
 
+function generateVowel(str : String) : String{
+    return str.replace("ae", "ä").replace("ue", "ü").replace("oe", "ö");
+}
+
+function generateVowelForAppointment(appointment : Array<Appointment>): Array<Appointment>{
+    let updatedAppointments : Array<Appointment> = [];
+
+    appointment.forEach(appointment => {
+        appointment = {...appointment};
+        appointment.place = generateVowel(appointment.place);
+        appointment.summary = generateVowel(appointment.summary);
+        appointment.title = generateVowel(appointment.title);
+        updatedAppointments.push(appointment);
+    })
+    return updatedAppointments;
+}
+
 export const appointmentReducer = createReducer(
     INITIAL_STATE,
-    on(loadAppointmentsSuccess, (state, {appointments}) => appointments),
+    on(loadAppointmentsSuccess, (state, {appointments}) => generateVowelForAppointment(appointments)),
     on(clear, (state) => INITIAL_STATE)
 )
--- src/app/state/class/class.reducer.ts
+++ src/app/state/class/class.reducer.ts
@@ -1,10 +1,28 @@
 import { createReducer, on } from "@ngrx/store";
+import { ClassSelectorComponent } from "src/app/component/stunden-overview/class-selector/class-selector.component";
 import { Class } from "src/app/model/class.model";
 import { loadClassesSuccess } from "./class.action";
 
 const INITIAL_STATE: Array<Class> = [];
 
+
+function generateVowel(str : String) : String{
+    return str.replace("ae", "ä").replace("ue", "ü").replace("oe", "ö");
+}
+
+
+function generateVowelForClasses(classes : Array<Class>): Array<Class>{
+    let updatedClasses : Array<Class> = [];
+    classes.forEach(cls => {
+        cls = {...cls};
+        cls.name = generateVowel(cls.name)
+        updatedClasses.push(cls);
+    })
+
+    return updatedClasses;
+}
+
 export const classReducer = createReducer(
     INITIAL_STATE,
-    on(loadClassesSuccess, (state, {classes}) => state.concat(classes))
+    on(loadClassesSuccess, (state, {classes}) => generateVowelForClasses(classes))
 )
